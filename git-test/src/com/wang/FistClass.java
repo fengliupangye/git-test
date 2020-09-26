01d3e73b4ce2e64bbc61d0b011852ef4f9a97a66
fengliupangye <wang_jingwei321@163.com>
2020-09-26 17:08:36 +0800

    本地工程添加到git

---
+++ git-test/src/com/wang/FistClass.java
@@ -0,0 +1,12 @@
+package com.wang;
+
+/**
+ * Created by Wangjingwei on 2020/9/26 16:56
+ */
+public class FistClass {
+    public static void main(String[] args) {
+        System.out.println("111111");
+        System.out.println("22222");
+        System.out.println("333333");
+    }
+}
